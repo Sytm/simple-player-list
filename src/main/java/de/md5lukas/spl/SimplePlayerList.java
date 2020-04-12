c5a713ecfd90a1afad3992584ff783f34a3bcba9
Lukas Planz <lukas.planz@web.de>
2020-04-12 18:01:33 +0200

    Added bStats metrics to plugin

--- src/main/java/de/md5lukas/spl/SimplePlayerList.java
+++ src/main/java/de/md5lukas/spl/SimplePlayerList.java
@@ -1,11 +1,14 @@
 package de.md5lukas.spl;
 
 import de.md5lukas.spl.config.SPLConfig;
+import org.bstats.bukkit.MetricsLite;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("ConstantConditions")
 public class SimplePlayerList extends JavaPlugin {
+
+    private static final int METRICS_PLUGIN_ID = 7109;
 
     private SPLConfig splConfig;
     private PlayerListUpdater playerListUpdater;
@@ -19,6 +22,8 @@ public class SimplePlayerList extends JavaPlugin {
         PluginCommand spl = getCommand("simpleplayerlist");
         spl.setExecutor(new SPLCommand(this));
         spl.setPermissionMessage(splConfig.getSPLMessages().getMainCommandNoPermission());
+
+        new MetricsLite(this, METRICS_PLUGIN_ID);
     }
 
     public void reloadSPLConfig() {
