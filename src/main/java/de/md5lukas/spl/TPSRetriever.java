93fb8fc030b25bfee3f0f5f179731987b17426d9
Lukas Planz <lukas.planz@web.de>
2021-08-12 20:25:21 +0200

    Updated NMS code to be hopefully as version stable as possible. Fixes #4
    - Bumped version to 1.3.1

--- src/main/java/de/md5lukas/spl/TPSRetriever.java
+++ src/main/java/de/md5lukas/spl/TPSRetriever.java
@@ -4,86 +4,28 @@ import org.bukkit.Bukkit;
 import org.bukkit.Server;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 
 public final class TPSRetriever {
 
-    private final String version;
-
-    public TPSRetriever() {
-        String packageName = Bukkit.getServer().getClass().getPackage().getName();
-        version = packageName.substring(packageName.lastIndexOf('.') + 1);
-
-        craftServer = getCB("CraftServer");
-        craftServerGetServer = getMethod(craftServer, "getServer");
-        Class<?> minecraftServer = getNMS("MinecraftServer");
-        minecraftServerRecentTPS = getField(minecraftServer, "recentTps");
-    }
-
-    private Class<?> getNMS(String name) {
-        try {
-            return Class.forName("net.minecraft.server." + version + "." + name);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Class<?> getCB(String name) {
-        try {
-            return Class.forName("org.bukkit.craftbukkit." + version + "." + name);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Method getMethod(Class<?> clazz, String name) {
-        try {
-            return clazz.getMethod(name);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Field getField(Class<?> clazz, String name) {
-        try {
-            return clazz.getField(name);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private final Class<?> craftServer;
-    private final Method craftServerGetServer;
-    private final Field minecraftServerRecentTPS;
-
-    private Object minecraftServerInstance = null;
-
-    private Object getMinecraftServer() {
-        if (minecraftServerInstance == null) {
-            Server server = Bukkit.getServer();
-
-            if (!craftServer.isInstance(server)) {
-                throw new RuntimeException("The server is not of type CraftServer");
-            }
-
-            Object craftServerInstance = craftServer.cast(server);
-
-            try {
-                minecraftServerInstance = craftServerGetServer.invoke(craftServerInstance);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return minecraftServerInstance;
-    }
+    private Object dedicatedServer = null;
+    private Field recentTpsField = null;
 
     public double[] getTPS() {
         try {
-            return (double[]) minecraftServerRecentTPS.get(getMinecraftServer());
-        } catch (IllegalAccessException e) {
+            if (recentTpsField == null) {
+                Server bukkitServer = Bukkit.getServer();
+
+                Field dedicatedServerField = bukkitServer.getClass().getDeclaredField("console");
+                dedicatedServerField.setAccessible(true);
+
+                dedicatedServer = dedicatedServerField.get(bukkitServer);
+                recentTpsField = dedicatedServer.getClass().getSuperclass().getDeclaredField("recentTps");
+                recentTpsField.setAccessible(true);
+            }
+
+            return (double[]) recentTpsField.get(dedicatedServer);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
             throw new RuntimeException(e);
         }
     }
